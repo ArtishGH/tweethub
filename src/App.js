b7fc4bc98de2966135d2be2bb98fa49c863a5e40
ArtishGH <b.prejs06@gmail.com>
2023-04-04 13:53:57 +0200

    Update pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import Contact from "./pages/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a>TweetHub</a>
-        <div className="front"></div>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to TweetHub</h1>
+    </div>
+  );
+};
+
+export default Home;
