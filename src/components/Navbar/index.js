8e415b255a5dbae92570d0e08b156184beeb07b4
LeonMielon <97433615+LeonMielon@users.noreply.github.com>
2023-04-28 20:58:36 +0200

    pasek zadań

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,31 +3,27 @@ import { Nav, NavLink, NavMenu } from "./NavbarElements";
 
 const Navbar = () => {
   return (
-    <div className="bar">
-      <h2>
-      <>
-        <Nav>
-          <NavMenu>
-            <NavLink to="/" activeStyle>
-              Home
-            </NavLink>
-            <NavLink to="/about" activeStyle>
-              About
-            </NavLink>
-            <NavLink to="/contact" activeStyle>
-              Contact
-            </NavLink>
-            <NavLink to="/login" activeStyle>
-              Login
-            </NavLink>
-            <NavLink to="/sign-up" activeStyle>
-              Sign Up
-            </NavLink>
-          </NavMenu>
-        </Nav>
-      </>
-      </h2>
-    </div>
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+          <NavLink to="/login" activeStyle>
+            Login
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
   );
 };
 
