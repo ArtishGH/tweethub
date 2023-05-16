fd1b55e8adff37e19166291909c43fb6aaf70886
LeonMielon <97433615+LeonMielon@users.noreply.github.com>
2023-05-16 12:52:35 +0200

    fianllyWorks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages";
-import About from "./pages/about";
-import Login from "./pages/login";
-import SignUp from "./pages/signup";
-import Contact from "./pages/contact";
-import Header from "./components/Navbar/header";
-
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<React.Fragment>
+			<Navbar/>
+		</React.Fragment>
+	);
 }
 
 export default App;
