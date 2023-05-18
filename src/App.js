a70891d811f7d96b1aeee8f9964f73ebbb53261a
miloZs <heromilo2006@gmail.com>
2023-05-18 03:49:14 +0200

    front first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+
 import React from 'react';
-import Navbar from "./components/Navbar";
+
+import "./styles/main.css";
+import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
+
+const Layaout = () => {
+	return(
+		<div>
+			<h1>Navbar</h1>
+			<Outlet></Outlet>
+		</div>		
+	);
+};
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layaout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/profile/:id",
+				element: <Profile />,
+			},
+		],
+	},
+]);
 
 function App() {
 	return (
-		<React.Fragment>
+		<div>
+			<RouterProvider router={router}></RouterProvider>
+			<React.Fragment>
 			<Navbar/>
-		</React.Fragment>
+			</React.Fragment>
+		</div>
 	);
 }
 
