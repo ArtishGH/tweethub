b3672e53f81250592e4e0e89d501807386b7464e
miloZs <heromilo2006@gmail.com>
2023-05-18 12:26:28 +0200

    backend #1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import userRoutes from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send("hello")
-})
+const connect = () => {
+    mongoose.set("strictQuery", false);
+    mongoose
+        .connect(process.env.MONGO)
+        .then(() => {console.log('connected to mongo')
+        })
+        .catch((err) => {
+            throw err;
+        });
+};
+
+app.use("/api/users", userRoutes);
 
 app.listen(8000, () => {
+    connect();
     console.log('listen to port 8000')
 });
