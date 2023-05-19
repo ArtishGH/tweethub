e563b13c384e63b03f1ecd6f6755d761ca67a4ce
ArtishGH <b.prejs06@gmail.com>
2023-05-19 20:27:57 +0200

    frontend code done
    everything works besides styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,56 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Explore from "./pages/Explore/Explore";
+import Signin from "./pages/Signin/Signin";
+import Navbar from "./components/Navbar/Navbar";
+import Error from "./pages/Error/Error";
+
+const Layout = () => {
+  return (
+    <div className="md:w-8/12 mx-auto">
+      <Navbar />
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Error />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/signout",
+        element: <Signin />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxIXjJls7nfTLEcDSmQLhPpT81g3vRXPk",
+  authDomain: "tweethub-215cf.firebaseapp.com",
+  projectId: "tweethub-215cf",
+  storageBucket: "tweethub-215cf.appspot.com",
+  messagingSenderId: "108158577204",
+  appId: "1:108158577204:web:bd5e32c189f97b1cf3f861"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
